f88840f5212cee8af67921ead7ff23fa145d6abc
GitWithLucky <luckyikenkwocha@gmail.com>
2022-10-21 07:23:25 -0700

    Custom Printf Fixed

--- get_size.c
+++ get_size.c
@@ -4,9 +4,9 @@
 * get_size - Calculates the size to cast the argument
 * @format: Formatted string in which to print the arguments
 * @i: List of arguments to be printed.
+*
 * Return: Precision.
 */
-
 int get_size(const char *format, int *i)
 {
 int curr_i = *i + 1;
@@ -16,13 +16,11 @@ if (format[curr_i] == 'l')
 size = S_LONG;
 else if (format[curr_i] == 'h')
 size = S_SHORT;
+
 if (size == 0)
 *i = curr_i - 1;
-
 else
-
 *i = curr_i;
 
 return (size);
-
 }
